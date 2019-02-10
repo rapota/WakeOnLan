3c8d0d9471852e9ff9c31fe080c56e721f06b5b3
Alexander Rapota <rapota84@gmail.com>
2019-02-10 12:32:36 +0200

    Added file logging.

--- WakeOnLan/Program.cs
+++ WakeOnLan/Program.cs
@@ -14,19 +14,22 @@ namespace WakeOnLan
             Log.Logger = new LoggerConfiguration()
                 .MinimumLevel.Debug()
                 .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3} {SourceContext}] {Message:lj}{NewLine}{Exception}")
+                .WriteTo.File("logs\\wol.txt")
                 .CreateLogger();
             ILogger logger = Log.ForContext(typeof(Program));
-            logger.Information("Starting.");
 
+            logger.Information("Application started.");
             AppDomain.CurrentDomain.UnhandledException += OnCurrentDomainUnhandledException;
 
             Parser.Default.ParseArguments<Options>(args)
                 .WithParsed(Wol.Run)
                 .WithNotParsed(HandleParseError);
+            logger.Information("Application stoped.");
         }
 
         private static void HandleParseError(IEnumerable<Error> errs)
         {
+            Log.Logger.Error("Parser errors:");
             foreach (Error error in errs)
             {
                 Log.Logger.Error(error.ToString());
